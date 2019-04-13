9bcf116d7c147f9eb15e0b400b219d9a383a9758
hyeribo <harmfuli59@gmail.com>
2019-04-13 22:12:49 +0900

    불필요한 import 삭제

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Row, Col, Tag } from 'antd';
-import axios from 'axios';
+import { Row, Col } from 'antd';
 import Masonry from 'react-masonry-component';
 
 import RandomButton from './RandomButton';
